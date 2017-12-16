13498078325d9d140332de350899b621d43d7036
diasaleh <dia-20111@hotmail.com>
2017-12-16 12:19:25 +0200

    good

--- codes/predict.py
+++ codes/predict.py
@@ -13,7 +13,7 @@ import random
 
 # size=int(sys.argv[3])+1
 cat1="test"
-fn=13
+fn=12
 farasaFeat = [6,18,26,38,42,54]
 charOut = [7,10]
 mhmj=[4,7]
@@ -32,8 +32,8 @@ sheet1_4 = book1_4.sheet_by_name('Sheet1')
 book1_5 = xlrd.open_workbook(str(cat1)+'_shell_output/'+str(cat1)+'all_entifakh_details.xlsx')
 sheet1_5 = book1_5.sheet_by_name('Sheet1')
 
-book1_6 = xlrd.open_workbook(str(cat1)+'_shell_output/'+str(cat1)+'PRON_cat.xlsx')
-sheet1_6 = book1_6.sheet_by_name('Sheet1')
+# book1_6 = xlrd.open_workbook(str(cat1)+'_shell_output/'+str(cat1)+'PRON_cat.xlsx')
+# sheet1_6 = book1_6.sheet_by_name('Sheet1')
 size1 = 1
 # print size1
 featuresCat1 = np.zeros(shape=(fn,size1))
@@ -47,6 +47,7 @@ for col in range(1,2):
 	featuresCat1[row][i] = (float(sheet1_2.cell_value(4, col)))
 	i+=1
 i=0
+# print featuresCat1
 
 for row in range(1,len(farasaFeat)+1):
 	# print row
@@ -57,6 +58,8 @@ for row in range(1,len(farasaFeat)+1):
 		i+=1
 	i=0
 i=0
+# print featuresCat1
+
 for row in range(7,9):
 	# print row
 	# print charOut[row-7]
@@ -67,6 +70,8 @@ for row in range(7,9):
 		i+=1
 	i=0
 i=0
+# print featuresCat1
+
 for row in range(9,11):
 	# print row
 	# print charOut[row-7]
@@ -77,6 +82,8 @@ for row in range(9,11):
 		i+=1
 	i=0
 i=0
+# print featuresCat1
+
 row = 11
 for col in range(1, 2):
 	# print col
@@ -85,24 +92,29 @@ for col in range(1, 2):
 	i+=1
 row = 12
 i=0
-for col in range(1, 2):
-	# print col
-	# print sheet1_4.cell_value(mhmj[row-9], col)
-	featuresCat1[row][i] = (float(sheet1_6.cell_value(3, col)))
-	i+=1
+# print featuresCat1
 
-f = open('NB_100_1000_hindawi_D500.pickle', 'rb')
+# for col in range(1, 2):
+# 	# print col
+# 	# print sheet1_4.cell_value(mhmj[row-9], col)
+# 	featuresCat1[row][i] = (float(sheet1_6.cell_value(3, col)))
+# 	i+=1
+# print featuresCat1
+from sklearn import svm
+
+f = open('NB_100_1000_hindawi_D1500.pickle', 'rb')
 clf = pickle.load(f)
 f.close()
 
-
+# print xCat1test
 
 xCat1test=np.dstack((featuresCat1)).reshape(-1,fn)
 print xCat1test
-print clf.predict_proba(xCat1test)[0][0]*100
-
-
 print clf.predict(xCat1test)
+
+print clf.predict_proba(xCat1test)
+
+
 
 
 
